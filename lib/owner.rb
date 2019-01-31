5368c600b081ee943bfc9ec7a683e32610569f05
Tracy Ho <tracyho0867@gmail.com>
2019-01-31 20:00:56 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,14 +2,13 @@ class Owner
   # attr_writer
   attr_reader :species
   attr_accessor :count
-  @@all = []
 
   def initialize()
     @count = @@all.length
     # @species = cat or dog or fish
     @@all << self
   end
-  # 
+  #
   # def count
   #   @count += 1
   # end
