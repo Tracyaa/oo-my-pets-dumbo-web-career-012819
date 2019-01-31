15afffbfdc30863492f3462dc39d8afdb6958ae0
Tracy Ho <tracyho0867@gmail.com>
2019-01-31 22:01:08 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -60,7 +60,7 @@ class Owner
 
   def sell_pets
     @pets = {:fishes => [], :dogs => [], :cats => []}
-    @@all.each {|pet| pet.mood = "nervoud"}
+    @@all.each {|pet| pet.mood = "nervous"}
   end
 
 
