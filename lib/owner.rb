697bbd972d084f5df53a1a0a8b66e78b2b9dbdb8
Tracy Ho <tracyho0867@gmail.com>
2019-01-31 22:30:41 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -69,7 +69,7 @@ class Owner
   end
 
   def list_pets
-    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets}"
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats]}"
 
       str += "2 fish, 3 dog(s), and 1 cat(s)."
 
